c713259aef72e37423cae9369b6849cc5fdb13a1
Deva010800 <devabrilsana@gmail.com>
2020-03-26 13:25:38 +0800

    tugas mandiri 1

---
+++ resources/views/admin/komunitas.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.admin')
+@section('content')
+<div class="content mt-3">
+<p style="color:black" align="left"> Nama   : Komang Deva Bayudi Brilsana</p>
+<p style="color:black" align="left"> Nim     : 1815051018</p>
+<p style="color:black" align="left"> Kelas  : Pendidikan Teknik Informatika/4D</p>
+</div>
+@endsection
